6dc01e2bb4e9422f83d9527d3cdd912b7df3f948
Nino Stephen <ninostephen.dev@gmail.com>
2023-06-24 11:45:39 +0530

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,165 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Nino Stephen',
+  tagline: 'Decoding the Mysteries of Weird Machines',
+  favicon: 'img/logos/svg/logo-color.svg',
+
+  // Set the production url of your site here
+  url: 'https://ninostephen.me',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ninostephen', // Usually your GitHub org/user name.
+  projectName: 'ninostephen.github.io', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ninostephen/ninostephen.github.io',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ninostephen/ninostephen.github.io',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 5,
+    },
+      // Replace with your project's social card
+      image: 'img/logos/svg/logo-color.svg',
+      navbar: {
+        title: 'Nino\'s Blog',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logos/png/logo-color.png',
+        },
+        items: [
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Series',
+          },
+          {to: '/support', label: 'Support Me!', position: 'left'},
+          {
+            href: 'https://github.com/ninostephen/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Support Me!',
+            items: [
+              {
+                html: "<a href='https://ko-fi.com/H2H0LNZW3' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi4.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>"
+              }, 
+              {
+                label: 'Click Here to Contact Me!',
+                to: 'mailto:contactform.yxu6l@aleeas.com',
+              },
+            ],
+
+          },
+          {
+            title: 'Discover',
+            items: [
+              {
+                label: 'Series',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+            ],
+          },
+          {
+            title: 'Connect',
+            items: [            
+              // {
+              //   label: 'Twitter',
+              //   href: 'https://twitter.com/_ninostephen_',
+              // },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ninostephen/',
+              },
+              // {
+              //   label: 'Email',
+              //   href: 'mailto:contactform.yxu6l@aleeas.com',
+              // },
+              {
+                label: 'Linkedin',
+                href: 'https://linkedin.com/in/ninostephen',
+              },
+            ],
+          },
+          //   {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: '/blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/ninostephen/',
+          //     },
+          //   ],
+          // },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Nino Stephen`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { useTrail, animated, useSpring } from "react-spring";
+import { Typography, Grid, Button } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { TestimonialSection } from '@site/src/components/HomepageFeatures';
+import Experience from "../components/HomepageFeatures/Experience";
+import Projects from "../components/HomepageFeatures/Projects";
+
+
+import styles from './index.module.css';
+
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="https://ko-fi.com/ninostephen">
+//             Love my work? Buy me a Ko-fi!
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+// function HomepageHeader() {
+export default function Home(): JSX.Element {
+  const context = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
+  const animatedHero = useSpring({
+    opacity: 1,
+    transform: "translateX(0)",
+    from: { opacity: 0, transform: "translateX(8em)" },
+    config: { mass: 2, tension: 260, friction: 30 },
+    delay: 600,
+  });
+  const animatedTexts = useTrail(5, {
+    from: { opacity: 0, transform: "translateY(3em)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    config: {
+      mass: 3,
+      friction: 45,
+      tension: 460,
+    },
+    delay: 200,
+  });
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Weird Machines are cool! <head />"
+    >
+      <Grid container style={{ padding: "5%" }} className="hero">
+        {/*Personal Intro */}
+        <Grid item xs={12} lg={6} className="homeIntro">
+          <animated.div style={animatedTexts[0]}>
+            <Typography variant="h2" gutterBottom>
+              Hello! I am
+              <span className="intro__name"> {siteConfig.title}</span>
+              {/* <Typography variant="body2">
+                <span>{siteConfig.tagline}</span>
+              </Typography> */}
+            </Typography>
+          </animated.div>
+          <animated.div style={animatedTexts[1]}>
+            <Typography variant="body1">
+
+              <strong>I empower millions through Code, Linux, and Security Expertise!</strong>
+              <br />
+              <br />
+              Greetings! I'm an aspiring technologist on a mission to revolutionize lives at scale.
+              <br />
+              <br />
+
+              Join me on this electrifying journey as we reimagine what's possible.
+              <br />
+              <br />
+
+              Are you ready to make a profound impact? Let's dive in!
+              {" "}
+            </Typography>
+          </animated.div>
+          &nbsp;
+          <animated.div style={animatedTexts[2]}>
+            <Typography variant="h6" gutterBottom>
+              My Skills:
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Golang, Linux, Docker, Kubernetes, Homelabbing and More!
+            </Typography>
+          </animated.div>
+          &nbsp;
+          {/* <animated.p style={animatedTexts[3]}>
+            <Button
+              style={{ textTransform: "none" }}
+              color="primary"
+              variant="outlined"
+              size="small"
+              href={resume}
+            >
+              My Resume
+            </Button>
+          </animated.p> */}
+          <SocialLinks animatedProps={animatedTexts[4]} />
+        </Grid>
+
+        <Grid item xs={12} lg={6} className="homeImg">
+          <animated.img
+            src={useBaseUrl('img/logos/png/hacker.png')}
+            style={animatedHero}
+          />
+        </Grid>
+      </Grid>
+      {/* Experiences section */}
+      <Grid>
+        <Experience />
+      </Grid>
+      {/* Projects section */}
+      <Grid>
+        <Projects />
+      </Grid>
+      <TestimonialSection />
+      {/* Contact form */}
+      <Grid>
+        {/* <Contact /> */}
+      </Grid>
+    </Layout>
+
+  );
+}
+
+function SocialLinks({ animatedProps, ...props }) {
+  return (
+    <animated.div className="social__links" style={animatedProps}>
+      <Grid container spacing={2}>
+        <Grid item>
+          <Typography display={"inline"} gutterBottom>
+            Social Media:
+          </Typography>
+        </Grid>
+        <Grid item>
+          <a href="https://www.linkedin.com/in/ninostephen/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </Grid>
+        <Grid item>
+          <a href="https://github.com/ninostephen">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </Grid>
+      </Grid>
+    </animated.div>
+  );
+}
+
+
+// export default function Home(): JSX.Element {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <Layout
+//       title={`Temp`}
+//       description="</head>">
+//       <HomepageHeader />
+//       <main>
+//         {/* <HomepageFeatures /> */}
+//         <TestimonialSection /> 
+//       </main>
+//     </Layout>
+//   );
+// }
