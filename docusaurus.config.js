c5fe5b7e18c5b7957c2590b626627033b37bccf4
Nino Stephen <ninostephen.dev@gmail.com>
2023-06-24 12:37:22 +0530

    making updates to make the site work again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,56 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Nino Stephen',
-  tagline: 'Decoding the Mysteries of Weird Machines',
-  favicon: 'img/logos/svg/logo-color.svg',
+  title: "Nino Stephen",
+  tagline: "Decoding the Mysteries of Weird Machines",
+  favicon: "img/logos/svg/logo-color.svg",
 
   // Set the production url of your site here
-  url: 'https://ninostephen.me',
+  url: "https://ninostephen.me",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ninostephen', // Usually your GitHub org/user name.
-  projectName: 'ninostephen.github.io', // Usually your repo name.
+  organizationName: "ninostephen", // Usually your GitHub org/user name.
+  projectName: "ninostephen.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ninostephen/ninostephen.github.io',
+          editUrl: "https://github.com/ninostephen/ninostephen.github.io",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ninostephen/ninostephen.github.io',
+          editUrl: "https://github.com/ninostephen/ninostephen.github.io",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -64,78 +62,69 @@ const config = {
       tableOfContents: {
         minHeadingLevel: 2,
         maxHeadingLevel: 5,
-    },
+      },
       // Replace with your project's social card
-      image: 'img/logos/svg/logo-color.svg',
+      image: "img/logos/svg/logo-color.svg",
       navbar: {
-        title: 'Nino\'s Blog',
+        title: "Nino's Blog",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logos/png/logo-color.png',
+          alt: "My Site Logo",
+          src: "img/logos/png/logo-color.png",
         },
         items: [
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Series',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Series",
           },
-          {to: '/support', label: 'Support Me!', position: 'left'},
+          { to: "/support", label: "Support Me!", position: "left" },
           {
-            href: 'https://github.com/ninostephen/',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/ninostephen/",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Support Me!',
+            title: "Support Me!",
             items: [
               {
-                html: "<a href='https://ko-fi.com/H2H0LNZW3' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi4.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>"
-              }, 
-              {
-                label: 'Click Here to Contact Me!',
-                to: 'mailto:contactform.yxu6l@aleeas.com',
-              },
-            ],
-
-          },
-          {
-            title: 'Discover',
-            items: [
-              {
-                label: 'Series',
-                to: '/docs/intro',
+                html: "<a href='https://ko-fi.com/H2H0LNZW3' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi4.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>",
               },
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Click Here to Contact Me!",
+                to: "mailto:contactform.yxu6l@aleeas.com",
               },
             ],
           },
           {
-            title: 'Connect',
-            items: [            
-              // {
-              //   label: 'Twitter',
-              //   href: 'https://twitter.com/_ninostephen_',
-              // },
+            title: "Discover",
+            items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/ninostephen/',
+                label: "Series",
+                to: "/docs/intro",
               },
-              // {
-              //   label: 'Email',
-              //   href: 'mailto:contactform.yxu6l@aleeas.com',
-              // },
               {
-                label: 'Linkedin',
-                href: 'https://linkedin.com/in/ninostephen',
+                label: "Blog",
+                to: "/blog",
+              },
+            ],
+          },
+          {
+            title: "Connect",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/ninostephen/",
+              },
+              {
+                label: "Linkedin",
+                href: "https://linkedin.com/in/ninostephen",
               },
             ],
           },
