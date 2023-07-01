dd4a0098ebe08851a62102a0788e110bd7617825
Nino Stephen <ninostephen.dev@gmail.com>
2023-07-01 09:02:34 +0530

    Bad luck

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,25 +17,6 @@ import Projects from "../components/HomepageFeatures/Projects";
 import styles from './index.module.css';
 
 // function HomepageHeader() {
-//   const {siteConfig} = useDocusaurusContext();
-//   return (
-//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-//       <div className="container">
-//         <h1 className="hero__title">{siteConfig.title}</h1>
-//         <p className="hero__subtitle">{siteConfig.tagline}</p>
-//         <div className={styles.buttons}>
-//           <Link
-//             className="button button--secondary button--lg"
-//             to="https://ko-fi.com/ninostephen">
-//             Love my work? Buy me a Ko-fi!
-//           </Link>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
-// function HomepageHeader() {
 export default function Home(): JSX.Element {
   const context = useDocusaurusContext();
   const { siteConfig } = useDocusaurusContext();
@@ -132,11 +113,10 @@ export default function Home(): JSX.Element {
       <Grid>
         <Projects />
       </Grid>
-      <TestimonialSection />
-      {/* Contact form */}
-      <Grid>
-        {/* <Contact /> */}
-      </Grid>
+      {/* <TestimonialSection /> */}
+      {/* <Grid> */}
+      {/* <Contact /> */}
+      {/* </Grid> */}
     </Layout>
 
   );
